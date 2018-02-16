dcdfa0553c89c50b54887cf6a531a317752eee7b
salibacristian <chriseze21@gmail.com>
2018-02-16 00:28:54 -0300

    calculo de importe OK

--- Estacionamiento/backEnd/Aplicacion/OperacionService.php
+++ Estacionamiento/backEnd/Aplicacion/OperacionService.php
@@ -74,43 +74,56 @@ class OperacionService extends Operacion //implements IApiUsable
     // }
      
      public function ModificarUno($request, $response, $args) {
-     $ArrayDeParametros = $request->getParsedBody();
-      // var_dump($ArrayDeParametros);die();
+       $ArrayDeParametros = $request->getParsedBody();
+        // var_dump($ArrayDeParametros);die();
+        $o=Operacion::TraerOperacionPorDominio($ArrayDeParametros['dominio']);
+        if($o != null){
+          $o->id_empleado_salida = $ArrayDeParametros['id_empleado_salida'];//sacarlo del session
 
-      $o=Operacion::TraerOperacionPorDominio($ArrayDeParametros['dominio']);
-      if($o != null){
-      //$o = new Operacion();
-      //$o->id=$ArrayDeParametros['id'];
-      //$o->nombre=$ArrayDeParametros['dominio'];
-      $o->id_empleado_salida = $ArrayDeParametros['id_empleado_salida'];//sacarlo del session
-      $today = getdate();
-      $o->fecha_hora_salida = $today->mday."/".$today->mon."/".$today->year." "
-      .$today->hours.":".$today->minutes;
+          //seteo hora local 
+          date_default_timezone_set('America/Argentina/Buenos_Aires');
+          $today = getdate();
+          //var_dump($today);
 
+          //GUARDO LA FECHA ACTUAL EN FORMATO PROPIO (dd/mm/yyyy hh:mm)
+          $o->fecha_hora_salida = $today['mday']."/".$today['mon']."/".$today['year']." "
+          .$today['hours'].":".$today['minutes'];
+          //var_dump($o->fecha_hora_salida);
+          
+          //...............................................................................
 
-      //format date dd/mm/yyyy hh:mm
+          //necesito la fecha de ingreso a datetime para sacar el diff 
+          $dateAndTime = explode(" ", $o->fecha_hora_ingreso);//separo hora de la fecha
+          $date = explode("/", $dateAndTime[0]);//separo dia,mes y año
+          $time = explode(":", $dateAndTime[1]);//separo hora y minuto
+          $day = intval($date[0],10);
+          $month = intval($date[1],10);
+          $year = intval($date[2],10);
+          $hour = intval($time[0],10);
+          $min = intval($time[1],10);
+          $mktime = mktime($hour,$min,0,$month,$day,$year);
+          $ingreso = new DateTime(date(DATE_ATOM,$mktime));
+          var_dump($ingreso);
 
-      $dateTime = explode(" ", $o->fecha_hora_ingreso);
-      $date = explode("/", $dateTime[0]);
-      $time = explode(":", $dateTime[1]);
-      $day = intval($date[0],10);
-      $month = intval($date[1],10);
-      $year = intval($date[2],10);
-      $hour = intval($time[0],10);
-      $min = intval($time[1],10);
-      $ingreso = new DateTime(mktime($hour,$min,0,$month,$day,$year));
-      $now = new DateTime(date(DATE_ATOM));
-      $dif = $now ->diff($ingreso);
-      $o->tiempo =  $dif->h;
-      $o->importe = $this->CalculateImport($o->tiempo);  
+          //...............................................................................
 
-    $resultado =$o->Modificar();    
-      $objDelaRespuesta= new stdclass();
-    //var_dump($resultado);
-    $objDelaRespuesta->resultado=$resultado;
-    return $response->withJson($objDelaRespuesta, 200); 
-    }	
-     return $response->getBody()->write("El auto no está"); 
+          //ahora que tengo la fecha de ingreso saco dif con now
+          $now = new DateTime(date(DATE_ATOM));
+          var_dump($now);
+          $diff = date_diff($ingreso, $now);
+          //var_dump($diff);
+          $o->tiempo =  $diff->h + ($diff->d * 24);
+          var_dump($o->tiempo);
+          $o->importe = $this->CalculateImport($o->tiempo);
+          var_dump($o->importe); die();
+
+          $resultado =$o->Modificar();    
+          $objDelaRespuesta= new stdclass();
+          //var_dump($resultado);
+          $objDelaRespuesta->resultado=$resultado;
+          return $response->withJson($objDelaRespuesta, 200); 
+        }	
+       return $response->getBody()->write("El auto no está"); 
     }
 
      function CalculateImport($hours){
